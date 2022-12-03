523ec40fcbb6e666284b1e2b6715ea3ed2ff6f4f
sevak <seva.kabrits@gmail.com>
2022-12-03 15:30:29 +0300

    init commit

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "serial.cpp"
+
+int main() {
+    std::cout << foo(5) << std::endl;
+    return 0;
+}
---
+++ serial.cpp
@@ -0,0 +1,7 @@
+//
+// Created by sevak on 12/3/2022.
+//
+
+int foo(int a){
+    return a+1;
+}
